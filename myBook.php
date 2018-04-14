9e877fdc4d24c2af2c08108bc82a8fe67a7db93a
NileshRPatil <31068788+NileshRPatil@users.noreply.github.com>
2018-04-14 13:55:30 +0530

    My Book is uploaded

---
+++ myBook.php
@@ -0,0 +1,271 @@
+
+<?php
+// Important Document
+
+********************* Important functions *********************
+
+// php
+1) error_reporting(level); level : error_reporting(E_ALL);,error_reporting(E_ERROR | E_WARNING | E_PARSE);ini_set("error_reporting", E_ALL);
+2) echo phpversion(); // get php version
+
+
+// JQUERY
+
+1) split() - jquery - split a sentence into array e.g. str.split(' ');
+2)	//How do I check if string contains substring?
+	if (str.indexOf("Yes") >= 0) or if (~str.indexOf("Yes"))
+	
+	if (str.toLowerCase().indexOf("yes") >= 0) or if (/yes/i.test(str)) // if case sensitive then
+
+
+
+
+******************* Important Links *********************
+
+// pagination
+ 
+http://www.dreamtemplate.com/dreamcodes/documentation/sliders.html
+	
+http://www.phpeasystep.com/phptu/29.html
+	
+--------------------------------------------------------------------------------------------------------------
+
+// State Cities in India 
+//mysql query for state, district, city in india
+  
+http://questionbox.in/india-state-district-sub-district-database-in-excel-csv-and-sql-query-format/
+http://www.c-sharpcorner.com/forums/sql-server-districts-table
+http://www.c-sharpcorner.com/forums/india-state-city-pin-code-database-sql-file
+https://www.daniweb.com/programming/databases/threads/416522/indian-state-and-its-city-database
+
+--------------------------------------------------------------------------------------------------------------
+
+
+// upload image without using formdata in jQuery
+
+http://stackoverflow.com/questions/5392344/sending-multipart-formdata-with-jquery-ajax	
+
+--------------------------------------------------------------------------------------------------------------
+
+// html form with validation
+http://formvalidation.io/examples/
+
+http://www.jqueryscript.net/form/Powerful-Form-Validation-Plugin-For-jQuery-Bootstrap-3.html
+
+https://1000hz.github.io/bootstrap-validator/#validator-fields
+
+http://www.formvalidator.net/
+
+--------------------------------------------------------------------------------------------------------------
+
+// softwares download 
+http://en.uptodown.com/windows/internet
+
+--------------------------------------------------------------------------------------------------------------
+
+//jquery plugins
+http://jquery-plugins.net/
+
+--------------------------------------------------------------------------------------------------------------
+
+// file uploader
+http://filer.grandesign.md/#demos
+
+[www.dropzonejs.com](http://www.dropzonejs.com)
+
+--------------------------------------------------------------------------------------------------------------
+
+// scroll 
+http://enscrollplugin.com/#documentation
+
+--------------------------------------------------------------------------------------------------------------
+
+// photoshop tutorial
+
+https://www.youtube.com/watch?v=x0TXU0saNtM&index=12&list=PLIr8j3Q0h3mHPSMnBiG-ZDH2Zg995-Pyd
+
+--------------------------------------------------------------------------------------------------------------
+
+//pagination
+http://www.phpeasystep.com/phptu/29.html
+
+// list view
+http://listjs.com/
+
+http://luis-almeida.github.io/jPages/mosaic.html
+
+http://jplist.com/demo
+
+//Multi Column Dropdown Box with jQuery and CSS3 
+http://www.jqueryscript.net/form/Multi-Column-Dropdown-Box-with-jQuery-CSS3-Multi-Column-Select.html
+
+// free icons
+http://www.flaticon.com/packs/office-11
+
+// toggle button 
+http://www.bootstraptoggle.com/
+
+// tool for display error log : [putty]
+
+// data tables with server side processing
+https://coderexample.com/datatable-demo-server-side-in-phpmysql-and-ajax/
+
+// drag and drop file upload 
+https://css-tricks.com/drag-and-drop-file-uploading/
+
+// PDO Tutorial
+https://phpdelusions.net/pdo
+
+// multiple select
+https://select2.github.io/examples.html
+https://silviomoreto.github.io/bootstrap-select/examples/#basic-examples
+http://www.jqueryscript.net/demo/jQuery-Multiple-Select-Plugin-For-Bootstrap-Bootstrap-Multiselect/
+
+// PHP mailer tutorial
+https://www.sitepoint.com/sending-emails-php-phpmailer/
+
+// mysql Tutorial
+http://www.mysqltutorial.org/
+
+https://www.techonthenet.com/mysql
+
+//check mobile device from php
+http://mobiledetect.net/
+
+// Android Link
+https://www.simplifiedcoding.net/firebase-cloud-messaging-tutorial-android/
+
+//date picker
+https://bootstrap-datepicker.readthedocs.io/en/latest/options.html
+
+//time picker
+https://weareoutman.github.io/clockpicker/
+
+//date range picker
+http://www.daterangepicker.com/
+
+// share on social media
+
+http://tools.sharethis.com/account/
+username nileshrpatil1811@gmail.com
+password nilesh3832
+
+<span class='st_sharethis_large' displayText='ShareThis'></span>
+<span class='st_facebook_large' displayText='Facebook'></span>
+<span class='st_email_large' displayText='Email'></span>
+<span class='st_twitter_large' displayText='Tweet'></span>
+<span class='st_googleplus_large' displayText='Google +'></span>
+<span class='st_whatsapp_large' displayText='WhatsApp'></span>
+
+copy the script tags and place them inside and at the end of your "head" tag.
+
+<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" id="st_insights_js" src="http://w.sharethis.com/button/buttons.js?publisher=0e84febd-8ba4-45ca-a323-dec6594ad488"></script>
+<script type="text/javascript">stLight.options({publisher: "0e84febd-8ba4-45ca-a323-dec6594ad488", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+
+// hover effects
+http://www.queness.com/post/16216/create-different-styles-of-hover-effects-with-css3-only
+
+// alert
+http://bootboxjs.com/examples.html
+
+http://alfredobarron.github.io/smoke/#/
+
+//resize image
+http://www.9lessons.info/2009/03/upload-and-resize-image-with-php.html#surveyGridOne
+
+// multi steps form
+https://www.thesoftwareguy.in/creating-responsive-multi-step-form-bootstrap-jquery/
+
+// facebook api
+https://www.youtube.com/watch?v=P9ktjGNeAzg&list=PLmpuiTmSb3xCLwwkpXeuJxwKyljf7e3Tx 
+
+https://www.youtube.com/watch?v=gNFQkVeOXa0
+
+sohaib ilyas github
+
+// Instagram API
+https://elfsight.com/blog/2016/05/how-to-get-instagram-access-token/
+
+get access token/  https://www.instagram.com/oauth/authorize/?client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&response_type=token
+
+//Social media share
+https://www.addthis.com/academy/setting-the-url-title-to-share/
+
+https://www.addtoany.com/buttons/customize/https://www.addtoany.com/buttons/customize/
+
+// PHP Tutorial
+http://www.codexworld.com
+
+// Regular expressions
+https://www.regex101.com
+
+// android Tutorial
+http://sampleprogramz.com/
+
+// Cronjob Setting
+http://www.thegeekstuff.com/2009/06/15-practical-crontab-examples
+
+http://crontab-generator.org/
+
+// Import Database From CSV file to MySql
+sqlilser.co.in
+
+// code igniter tutorial
+https://www.youtube.com/watch?v=c_eROQKnkfc&list=PLHFGzOr0F8DI5IBxWWzHvdUL_HmWUAqg2&index=46
+
+// HTML material design
+https://getmdl.io/components/index.html#textfields-section
+http://materializecss.com/forms.html
+
+// Jquery Plugins
+http://www.jqueryrain.com/demo/jquery-autocomplete/
+
+//bootstrap notify
+http://bootstrap-notify.remabledesigns.com/
+
+
+******************* Important Notes *********************
+
+Note :  after ajax call there must be RETURN FALSE otherwise it gives error [object Object];
+
+
+******************* New Concepts *********************
+1]  Tinymcr text editor	
+
+
+******************* Error *********************
+
+1) //jquery is not defined
+http://stackoverflow.com/questions/2194992/jquery-is-not-defined
+
+2) how to debug jquery
+ console.log(c);
+
+3) PHP 
+//  http://stackoverflow.com/questions/4314392/send-parameters-to-a-url-and-get-output-from-that-page
+
+Varad
+Anwik
+
+
+//Hacker rank
+input 
+3
+1 2 3
+fscanf(STDIN, "%d\n", $n);
+fscanf(STDIN,"%[^\n]s",$a);
+input
+2
+18
+40
+fscanf(STDIN, "%s", $n);
+for($i=0;$i<$n;$i++)
+{
+    fscanf(STDIN, "\n%s", $seat_no);
+}
+
+45
+45
+45
+45
