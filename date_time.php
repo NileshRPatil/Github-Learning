a52bc4af8206e47e8b739211aef91d5b4afc21ac
NileshRPatil <nileshrpatil1811@gmail.com>
2018-06-19 18:19:42 +0530

    set timezone to date

--- date_time.php
+++ date_time.php
@@ -23,7 +23,54 @@ $date = new DateTime;
 
 // $date->sub(new DateInterval('P10DT2H'));
 
-$newdate = clone $date;
-$newdate = $newdate->modify("+2 day");
+// $newdate = clone $date;
+// $newdate = $newdate->modify("+2 day");
 
-var_dump($newdate);
+
+
+//difference
+
+// $myBirthDay = (new DateTime("18 November 1987"))->setTime(07,30,15);
+
+// $timeUntilBirthDay = $date->diff($myBirthDay);
+
+// echo $timeUntilBirthDay->format("%y years %m months %d days");
+
+
+//compare
+
+// $date1 = new DateTime("2018-06-19 12:00:00");
+
+// $date2 = new DateTime("2018-06-01");
+
+// if($date1 > $date2)
+// {
+// 	echo "Yes";
+// }
+
+
+// if((int)$date->format("Y") > 2017 )
+// {
+// 	echo "Yes";
+// }
+
+
+//set timezone
+
+// $date->setTimezone(new DateTimeZone('Europe/London'));
+
+// $date = new DateTime('Now', new DateTimeZone('Europe/London'));
+
+// var_dump($date);
+
+// var_dump(DateTimeZone::listIdentifiers());
+
+
+// $timezone = 'Europe/London';
+
+// date_default_timezone_set($timezone);
+
+// $date = new DateTime();
+
+// var_dump($date);
+
